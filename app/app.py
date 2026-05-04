9f62af0249475bb56731975e91df9f4e6a44fe86
brettmt2 <btpeach.2@gmail.com>
2026-05-04 16:06:23 -0400

    redis cache layer for summary endpoint

---
+++ app/app.py
@@ -0,0 +1,45 @@
+import json
+from contextlib import asynccontextmanager
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import redis
+
+from app.pawncard import get_player_summary
+
+load_dotenv(override=False)
+
+r: redis.Redis = None
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    # load Redis manager
+    global r
+    r = redis.Redis(host='localhost', port=6379, db=0)
+
+    yield
+
+    r.close()
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/summaries/{username}")
+def get_summary(username: str):
+
+    cached = r.get(f'summary/{username}')
+    if cached:
+        return json.loads(cached)
+    
+    data = get_player_summary(username=username)
+    r.setex(f'summary/{username}', 30, json.dumps(data))
+    
+    return data
+
