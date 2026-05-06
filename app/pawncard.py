52ea5048cbe67ea55f1f5d17606691e23e1c4c56
brettmt2 <btpeach.2@gmail.com>
2026-05-06 17:57:10 -0400

    append feed logic base

--- app/pawncard.py
+++ app/pawncard.py
@@ -1,6 +1,8 @@
 import os
 import httpx
 import json
+from datetime import datetime
+import asyncio
 
 async def get_player_summary_stats(client: httpx.AsyncClient, username: str):
     headers = {'User-Agent': 'Mozilla/5.0'}
@@ -52,11 +54,49 @@ async def get_player_summary(client: httpx.AsyncClient, username: str):
     
 
 async def append_feed(feed: list, client: httpx.AsyncClient, username: str):
-    if feed:
-        feed.append({'feed-content': 1})
+    feed_item = {}
+    now = datetime.now()
+    url = f'https://api.chess.com/pub/player/{username}/games/{now.year}/{now.month:02d}'
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    result = await client.get(url, headers=headers)
+
+    data = result.json()
+    games: list = data.get('games', [])
+    games.reverse()
+
+    if not games:
         return feed
+    
+    # get the most recent win and append to feed
+    for game in games:
+        white = game['white']
+        black = game['black']
+
+        if (white['result'] == 'win' and white['username'] == username) \
+            or (black['result'] == 'win' and black['username'] == username):
+
+            # feed_item['pgn'] = game['pgn']
+
+            if white['result'] == 'win':
+                feed_item['win_color'] = 'white'
+                feed_item['opponent'] = black['username']
+            
+            if black['result'] == 'win':
+                feed_item['win_color'] = 'black'
+                feed_item['opponent'] = white['username']
+
+            feed_item['accuracies'] = game['accuracies']
+            
+            break
+
+    if feed_item and feed is not None:
+        feed.append(feed_item)
         
-    return [{'feed-content': 1}]
+    if feed_item and feed is None:
+        feed = [feed_item]
+
+    return feed
 
 async def get_player_feed(s3, client: httpx.AsyncClient, username: str, append: bool = True):
     paginator = s3.get_paginator('list_objects_v2')
@@ -84,3 +124,11 @@ async def get_player_feed(s3, client: httpx.AsyncClient, username: str, append: 
         return updated_feed
 
     return feed
+
+if __name__ == "__main__":
+    async def main():
+        async with httpx.AsyncClient() as client:
+            res = await append_feed(None, client, "peach_02")
+            print(res)
+    
+    asyncio.run(main())
