c3d9c44ea0c4f5308a6a577fbeff7e402a49132c
brettmt2 <btpeach.2@gmail.com>
2026-05-09 10:54:54 -0400

    remove localhost configuration on api bridge

--- web/api.js
+++ web/api.js
@@ -1,6 +1,8 @@
 export async function getUserData(username) {
-    const res = await fetch(`http://localhost:3000/data/${username}`);
+    const res = await fetch(`/data/${username}`);
     const data = await res.json();
 
     return data;
 }
+
+// http://localhost:3000
