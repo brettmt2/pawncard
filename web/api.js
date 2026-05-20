4159ffc46fbacf84ce629cdafb9b8b39fe49cc66
brettmt2 <btpeach.2@gmail.com>
2026-05-20 16:38:46 -0400

    add endpoint and design of analysis

--- web/api.js
+++ web/api.js
@@ -5,4 +5,11 @@ export async function getUserData(username) {
     return data;
 }
 
+export async function analyzeGame(username, game_id) {
+    const res = await fetch(`/analysis/${username}/${game_id}`);
+    const data = await res.json();
+
+    return data;
+}
+
 // http://localhost:3000
