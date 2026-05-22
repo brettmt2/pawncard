b00ad9e6f539234f21d92847473eeb8652e503e2
brettmt2 <btpeach.2@gmail.com>
2026-05-22 15:11:21 -0400

    get pgn from s3 bucket using passed in key

--- app/analyze.py
+++ app/analyze.py
@@ -1,8 +1,17 @@
 import json
-
-def analyze_pgn(username: str, game_id: str):
-    pass
+import os
 
 def get_pgn(s3, username: str, game_id: str):
-    response = s3.get_object(Bucket='your-bucket-name', Key=username)
+    response = s3.get_object(Bucket=os.getenv('FEEDS_BUCKET_NAME'), Key=username)
     feed_content = json.loads(response['Body'].read().decode('utf-8'))
+    
+    pgn = None
+    for f in feed_content:
+        if f['feed_id'] == game_id:
+            pgn = f['pgn'] if f['pgn'] else None
+
+    return pgn
+
+
+def analyze_pgn(pgn: str):
+    pass
