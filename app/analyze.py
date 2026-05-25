adb447deba2e57f776e7a1a6af2cc19ebbd5354c
brettmt2 <btpeach.2@gmail.com>
2026-05-25 12:22:50 -0400

    OOP for analysis engine and add stockfish

--- app/analyze.py
+++ app/analyze.py
@@ -1,27 +1,37 @@
 import json
 import os
+from stockfish import Stockfish
 
-def get_pgn(s3, username: str, game_id: str):
-    response = s3.get_object(Bucket=os.getenv('FEEDS_BUCKET_NAME'), Key=f'feeds/{username}')
-    feed_content = json.loads(response['Body'].read().decode('utf-8'))
+class analysis_engine:
+    def __init__(self, sf: Stockfish, s3, username: str, game_id: str):
+        self.sf = sf
+        self.s3 = s3
+        self.username = username
+        self.game_id = game_id
+
+    def get_pgn(self):
+        response = self.s3.get_object(Bucket=os.getenv('FEEDS_BUCKET_NAME'), Key=f'feeds/{self.username}')
+        feed_content = json.loads(response['Body'].read().decode('utf-8'))
+        
+        pgn = None
+        for f in feed_content:
+            if f['feed_id'] == self.game_id:
+                pgn = f['pgn'] if f['pgn'] else None
+
+        return pgn
+
+    def analyze_pgn(self, pgn):
+        return ['test', pgn[:10]]
+
+    def analyze(self):
+        pgn = self.get_pgn()
+
+        if pgn:
+            response = self.analyze_pgn(pgn)
+        else: # the game id from chesscom needs to be already analyzed on their server for a pgn to be in the body
+            response = None
+
+        return response
     
-    pgn = None
-    for f in feed_content:
-        if f['feed_id'] == game_id:
-            pgn = f['pgn'] if f['pgn'] else None
-
-    return pgn
-
-def analyze_pgn(pgn):
-    return ['test', pgn[:10]]
-
-def analyze(s3, username: str, game_id: str):
-    pgn = get_pgn(s3=s3, username=username, game_id=game_id)
-
-    if pgn:
-        response = analyze_pgn(pgn)
-    else: # the game id from chesscom needs to be already analyzed on their server for a pgn to be in the body
-        response = None
-
-    return response
-
+    def version(self):
+        return self.sf.get_stockfish_major_minor_version()
