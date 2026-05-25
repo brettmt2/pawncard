c5ba162f4cc04a0c8c1e752caf4f41939e0c7e8a
brettmt2 <btpeach.2@gmail.com>
2026-05-25 12:35:37 -0400

    clean API pgn format into standard pgn

--- app/analyze.py
+++ app/analyze.py
@@ -1,6 +1,7 @@
 import json
 import os
 from stockfish import Stockfish
+import re
 
 class analysis_engine:
     def __init__(self, sf: Stockfish, s3, username: str, game_id: str):
@@ -8,6 +9,30 @@ class analysis_engine:
         self.s3 = s3
         self.username = username
         self.game_id = game_id
+
+    def clean_pgn(self, pgn: str):
+        headers = {"Event", "Site", "Date", "Round", "White", "Black", "Result"}
+
+        header_lines = []
+        for line in pgn.splitlines():
+            match = re.match(r'^\[(\w+)\s+".*"\]$', line)
+            if match and match.group(1) in headers:
+                header_lines.append(line)
+        
+        moves_section = pgn.split("\n\n", 1)[1].strip()
+
+        # remove clock
+        moves_clean = re.sub(r'\s*\{[^}]*\}', '', moves_section)
+
+        # remove the ... from black's moves
+        moves_clean = re.sub(r'\d+\.\.\.', '', moves_clean)
+
+        # remove extra whitespace
+        moves_clean = re.sub(r' +', ' ', moves_clean).strip()
+
+        pgn = "\n".join(header_lines) + "\n\n" + moves_clean
+
+        return pgn
 
     def get_pgn(self):
         response = self.s3.get_object(Bucket=os.getenv('FEEDS_BUCKET_NAME'), Key=f'feeds/{self.username}')
@@ -18,7 +43,7 @@ class analysis_engine:
             if f['feed_id'] == self.game_id:
                 pgn = f['pgn'] if f['pgn'] else None
 
-        return pgn
+        return self.clean_pgn(pgn)
 
     def analyze_pgn(self, pgn):
         return ['test', pgn[:10]]
